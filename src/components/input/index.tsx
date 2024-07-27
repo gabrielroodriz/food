9d237377cd136606f4c72a252d40030aa5205b9c
Gabriel Rodrigues <gabrilroodriz@gmail.com>
2024-07-27 16:44:04 -0300

    update

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -5,7 +5,7 @@ import { TextInput, View } from 'react-native';
 export default function Input() {
 	return (
 		<View className='w-full flex-row border border-slate-500 h-14 rounded-full items-center gap-2 px-4 bg-transparent'>
-			<Feather name='search' size={24} color='#64748b'></Feather>
+			<Feather name='search' size={24} color='#64748b' />
 			<TextInput
 				placeholder='Encontre o que mais te agrada..'
 				className='w-full h-full flex-1 bg-transparent'
