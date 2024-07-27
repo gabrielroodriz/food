74de81b747a38397c6ec0946bc210e408e143ac1
Gabriel Rodrigues <gabrilroodriz@gmail.com>
2024-07-27 16:33:37 -0300

    feat(app): Create banner component

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -1,30 +1,25 @@
 import React from 'react';
 import { Image, Pressable, View } from 'react-native';
 import PagerView from 'react-native-pager-view';
+
 export default function Banner() {
+	const assets = [
+		{ key: 1, media: require('../../assets/banner1.png') },
+		{ key: 2, media: require('../../assets/banner2.png') },
+	];
+
 	return (
-		<View className='w-full h-36 rounded-2xl mt-5 mb-4'>
+		<View className='w-full h-36 md:h-60 rounded-2xl mt-5 mb-4'>
 			<PagerView style={{ flex: 1 }} initialPage={0} pageMargin={14}>
-				<Pressable
-					className='w-full h-36 rounded-2xl'
-					key='1'
-					onPress={() => console.log('click event')}
-				>
-					<Image
-						source={require('../../assets/banner1.png')}
-						className='w-full h-36 rounded-2xl'
-					/>
-				</Pressable>
-				<Pressable
-					className='w-full h-36 rounded-2xl'
-					key='2'
-					onPress={() => console.log('click event')}
-				>
-					<Image
-						source={require('../../assets/banner2.png')}
-						className='w-full h-36 rounded-2xl'
-					/>
-				</Pressable>
+				{assets.map(({ key, media }) => (
+					<Pressable
+						className='w-full h-36 md:h-60 rounded-2xl'
+						key={key}
+						onPress={() => console.log(`event click media: ${key}`)}
+					>
+						<Image source={media} className='w-full h-36 md:h-60 rounded-2xl' />
+					</Pressable>
+				))}
 			</PagerView>
 		</View>
 	);
